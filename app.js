92af308d94431ad08d81e9bcd9f1ce5b10c88479
7soon66 <68373803+7soon66@users.noreply.github.com>
2024-11-19 16:07:50 +0300

    updated level2

--- app.js
+++ app.js
@@ -2,25 +2,55 @@
 
 const cards = document.querySelectorAll('.card')
 const winScreen = document.querySelector('.win-screen')
-const nextLevelButton = document.querySelector('.next-level')
 const mainMenuButton = document.querySelector('.main-menu')
 let messageEl = document.querySelector('.message')
+let nextLevelButton = document.getElementById('next-level')
 let flippedCards = []
+const timerElement = document.getElementById('timer')
+let timeLeft = 30
+let isTimerStart = false
+// nextLevelButton.disabled = true
 
 /*---------------------variables---------------------*/
+
+function startTimer() {
+  const timerInterval = setInterval(() => {
+    timeLeft--
+    timerElement.textContent = timeLeft
+
+    if (timeLeft === 0) {
+      clearInterval(timerInterval)
+
+      // Handle time's up scenario, e.g., end the game
+      messageEl.innerHTML =
+        '<p>Times up you loss, press restart button to repeat this level</p>'
+      cards.forEach((card) => card.classList.add('disabled'))
+      startTimer = true
+    }
+  }, 1000)
+}
 
 const resetGame = () => {
   cards.forEach((card) => {
     card.classList.remove('flipped', 'matched')
   })
-  ;(flippedCards = []), (messageEl.innerHTML = ' ')
+  ;(flippedCards = []),
+    (messageEl.innerHTML = ' '),
+    cards.forEach((card) => card.classList.remove('disabled')),
+    (timeLeft = 30),
+    (timerElement.textContent = timeLeft)
 }
 cards.forEach((card) => {
   card.addEventListener('click', () => {
     if (flippedCards.length < 2 && !card.classList.contains('flipped')) {
       card.classList.toggle('flipped')
       flippedCards.push(card)
-
+      console.log(isTimerStart)
+      if (isTimerStart) {
+      } else {
+        isTimerStart = true
+        startTimer()
+      }
       if (flippedCards.length === 2) {
         setTimeout(() => {
           if (flippedCards[0].id === flippedCards[1].id) {
@@ -44,6 +74,8 @@ const resetButton = document.querySelector('.reset')
 const checkWin = () => {
   if (Array.from(cards).every((card) => card.classList.contains('matched'))) {
     messageEl.innerHTML = '<p>you are a winner !!!</p>'
+    // clearInterval(timerInterval) it must stop the timing
+    // nextLevelButton.disabled = false
   }
 }
 
